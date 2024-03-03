a1f362c0204bdd198ae1839265ba3a34b3bbe37b
Borbos0 <nbmvc021@mail.ru>
2024-03-03 17:50:53 +0300

    fixes

--- renderLogin.js
+++ renderLogin.js
@@ -4,42 +4,43 @@ const elementList = document.getElementById("list");
 
 // Кнопка "Войти" в авторизации
 export function renderAuthorizationButton(comments) {
-	const authorizationButtonIn = document.getElementById(
-		"authorization-button-in"
-	);
-	const authorizationLogin = document.getElementById("add-login");
-	const authorizationPassword = document.getElementById("add-password");
-	authorizationButtonIn.addEventListener("click", () => {
-		authorizationLogin.classList.backgroundColor = "";
-		authorizationPassword.classList.backgroundColor = "";
-		if (authorizationLogin.value === "") {
-			authorizationLogin.classList.backgroundColor = "red";
-			return;
-		}
-		if (authorizationPassword.value === "") {
-			authorizationPassword.classList.backgroundColor = "red";
-			return;
-		}
-		initPostAuth(authorizationLogin.value, authorizationPassword.value)
-			.then((responseData) => {
-				setToken(responseData.user.token);
-				return renderComments(comments, responseData.user.name);
-			})
-			.catch((error) => {
-				console.log(error);
-				return alert("Неправильное имя пользователя или пароль");
-			});
-	});
+    const authorizationButtonIn = document.getElementById(
+        "authorization-button-in"
+    );
+    const authorizationLogin = document.getElementById("add-login");
+    const authorizationPassword = document.getElementById("add-password");
+    authorizationButtonIn.addEventListener("click", () => {
+        authorizationLogin.classList.backgroundColor = "";
+        authorizationPassword.classList.backgroundColor = "";
+        if (authorizationLogin.value === "") {
+            authorizationLogin.classList.backgroundColor = "red";
+            return;
+        }
+        if (authorizationPassword.value === "") {
+            authorizationPassword.classList.backgroundColor = "red";
+            return;
+        }
+        initPostAuth(authorizationLogin.value, authorizationPassword.value)
+            .then((responseData) => {
+                setToken(responseData.user.token);
+                return renderComments(comments, responseData.user.name);
+            })
+            .catch((error) => {
+                console.log(error);
+                return alert("Неправильное имя пользователя или пароль");
+            });
+    });
 }
 export function renderAuthorizationPage() {
-	const commentHtml = `<div class="comment">
+    const commentHtml = `<div class="comment">
           <div class="registration">
             <h2>Форма входа</h2>
             <input type="text" id="add-login" placeholder="Введите логин">
             <input type="password" id="add-password" placeholder="Введите пароль">
             <button id="authorization-button-in">Войти</button>
+            <button id="registration-button-in">Регистрация</button>
           </div>
         </div>`;
-	elementList.innerHTML = commentHtml;
-	initEventListeners();
+    elementList.innerHTML = commentHtml;
+    initEventListeners();
 }
